daa7fac45483803023549444bdb04a1460abc663
Dani Michel <dani.michel@smartprimes.ch>
2014-02-18 21:25:26 +0100

    AsNullable() extension added for value-typed maybes.

---
+++ Sources/Belt/Maybe/MaybeExtensions.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MaybeExtensions.cs" company="Dani Michel">
+//   Dani Michel 2013
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Belt.Maybe
+{
+    public static class MaybeExtensions
+    {
+        public static T? AsNullable<T>(this IMaybe<T> maybe) where T : struct
+        {
+            return maybe.Exists ? maybe.It : (T?)null;
+        }
+    }
+}
