e0c0fd2436b6ea381ffc0b35e68cbe7267cf78d2
Dani Michel <dani.michel@smartprimes.ch>
2014-02-18 07:32:33 +0100

    Solution created and set up.

---
+++ Sources/VersionInfo.cs
@@ -0,0 +1,12 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="VersionInfo.cs" company="Dani Michel">
+//   Dani Michel 2013
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyCompany("TheDmi")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyInformationalVersion("Belt 0.1.0")]
