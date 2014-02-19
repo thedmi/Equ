ad1b41a883f769f147aa0f86dfc939e031da147c
Dani Michel <dani.michel@smartprimes.ch>
2014-02-19 07:47:43 +0100

    SingleAsMaybe() and FirstAsMaybe() extensions for IEnumerable implemented.

---
+++ Sources/Belt/Maybe/EnumerableExtensions.cs
@@ -0,0 +1,69 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EnumerableExtensions.cs" company="Dani Michel">
+//   Dani Michel 2013
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Belt.Maybe
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Belt.FinalList;
+    using Belt.Guard;
+
+    public static class EnumerableExtensions
+    {
+        public static IMaybe<T> FirstAsMaybe<T>(this IEnumerable<T> source)
+        {
+            try
+            {
+                return Maybe.Is(source.First());
+            }
+            catch (InvalidOperationException)
+            {
+                return Maybe.Empty<T>();
+            }
+        }
+
+        public static IMaybe<T> FirstAsMaybe<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            try
+            {
+                return Maybe.Is(source.First(predicate));
+            }
+            catch (InvalidOperationException)
+            {
+                return Maybe.Empty<T>();
+            }
+        } 
+
+        public static IMaybe<T> SingleAsMaybe<T>(this IEnumerable<T> source)
+        {
+            var firstTwo = source.Take(2).ToFinalList();
+            return SingleAsMaybeCandidateToResult(firstTwo);
+        }
+
+        public static IMaybe<T> SingleAsMaybe<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            var firstTwoMatching = source.Where(predicate).Take(2).ToFinalList();
+            return SingleAsMaybeCandidateToResult(firstTwoMatching);
+        }
+
+        private static IMaybe<T> SingleAsMaybeCandidateToResult<T>(IFinalList<T> firstTwo)
+        {
+            Guard.Precondition(firstTwo.Count <= 2);
+
+            switch (firstTwo.Count)
+            {
+                case 0:
+                    return Maybe.Empty<T>();
+                case 1:
+                    return Maybe.Is(firstTwo[0]);
+                default:
+                    throw new InvalidOperationException("Source contains more than one element");
+            }
+        }
+    }
+}
---
+++ Sources/BeltTest/MaybeEnumerableExtensionsTest.cs
@@ -0,0 +1,93 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MaybeEnumerableExtensionsTest.cs" company="Dani Michel">
+//   Dani Michel 2013
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BeltTest
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Belt.Maybe;
+
+    using Xunit;
+
+    public class MaybeEnumerableExtensionsTest
+    {
+        [Fact]
+        public void FirstAsMaybeReturnsEmptyForEmptyList()
+        {
+            var result = new List<int>().FirstAsMaybe();
+            Assert.True(result.IsEmpty);
+        }
+
+        [Fact]
+        public void FirstAsMaybeWithPredicateReturnsEmptyForEmptyList()
+        {
+            var result = new List<int> { 2, 4 }.FirstAsMaybe(IsOdd);
+            Assert.True(result.IsEmpty);
+        }
+
+        [Fact]
+        public void FirstAsMaybeReturnsTheFirstValueWhenTheInputIsNonempty()
+        {
+            var result = new List<int> { 2, 4 }.FirstAsMaybe();
+            Assert.Equal(2, result.It);
+        }
+
+        [Fact]
+        public void FirstAsMaybeWithPredicateReturnsTheFirstMatchWhenThereIsOne()
+        {
+            var result = new List<int> { 2, 3, 5 }.FirstAsMaybe(IsOdd);
+            Assert.Equal(3, result.It);
+        }
+
+        [Fact]
+        public void SingleAsMaybeReturnsEmptyForEmptyList()
+        {
+            var result = new List<int>().SingleAsMaybe();
+            Assert.True(result.IsEmpty);
+        }
+
+        [Fact]
+        public void SingleAsMaybeReturnsTheSingleValueWhenTheInputIsNonempty()
+        {
+            var result = new List<int> { 7 }.SingleAsMaybe();
+            Assert.Equal(7, result.It);
+        }
+
+        [Fact]
+        public void SingleAsMaybeThrowsWhenTheInputIsLargerThanOneElement()
+        {
+            var list = new List<int> { 4, 7 };
+            Assert.Throws<InvalidOperationException>(() => list.SingleAsMaybe());
+        }
+
+        [Fact]
+        public void SingleAsMaybeWithPredicateReturnsEmptyForNoMatch()
+        {
+            var result = new List<int> { 2, 4, 6 }.SingleAsMaybe(IsOdd);
+            Assert.True(result.IsEmpty);
+        }
+
+        [Fact]
+        public void SingleAsMaybeWithPredicateReturnsTheSingleValueThatMatches()
+        {
+            var result = new List<int> { 2, 3, 4, 6 }.SingleAsMaybe(IsOdd);
+            Assert.Equal(3, result.It);
+        }
+
+        [Fact]
+        public void SingleAsMaybeThrowsWhenTheThereIsMoreThanOneMatch()
+        {
+            var list = new List<int> { 4, 7, 9 };
+            Assert.Throws<InvalidOperationException>(() => list.SingleAsMaybe(IsOdd));
+        }
+
+        private static bool IsOdd(int i)
+        {
+            return i % 2 == 1;
+        }
+    }
+}
