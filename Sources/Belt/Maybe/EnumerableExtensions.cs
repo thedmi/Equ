a46ea0a05a141e619dd8c0bb142f2336fb39e5d0
Dani Michel <dani.michel@smartprimes.ch>
2014-03-27 14:16:25 +0100

    API documentation added to the extensions that provide IMaybe wrappers.

--- Sources/Belt/Maybe/EnumerableExtensions.cs
+++ Sources/Belt/Maybe/EnumerableExtensions.cs
@@ -15,6 +15,10 @@ namespace Belt.Maybe
 
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Returns the first item in the sequence wrapped in a <see cref="IMaybe{T}"/>, or an empty <see cref="IMaybe{T}"/> 
+        /// if the sequence is empty.
+        /// </summary>
         public static IMaybe<T> FirstAsMaybe<T>(this IEnumerable<T> source)
         {
             try
@@ -27,6 +31,10 @@ namespace Belt.Maybe
             }
         }
 
+        /// <summary>
+        /// Returns the first item in the sequence that matches the given predicate wrapped in a <see cref="IMaybe{T}"/>, 
+        /// or an empty <see cref="IMaybe{T}"/>  if no item matches.
+        /// </summary>
         public static IMaybe<T> FirstAsMaybe<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
             try
@@ -37,14 +45,27 @@ namespace Belt.Maybe
             {
                 return Maybe.Empty<T>();
             }
-        } 
+        }
 
+        /// <summary>
+        /// Returns the one and only item in the sequence wrapped in a <see cref="IMaybe{T}"/>, or an empty <see cref="IMaybe{T}"/> 
+        /// if the sequence is empty. If the sequence contains more than one item, an <see cref="InvalidOperationException"/> is
+        /// thrown.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">There was more than one item in the sequence.</exception>
         public static IMaybe<T> SingleAsMaybe<T>(this IEnumerable<T> source)
         {
             var firstTwo = source.Take(2).ToFinalList();
             return SingleAsMaybeCandidateToResult(firstTwo);
         }
 
+        /// <summary>
+        /// Returns the one and only item in the sequence that matches the given predicate wrapped in a <see cref="IMaybe{T}"/>, 
+        /// or an empty <see cref="IMaybe{T}"/> if there is none. 
+        /// If the sequence contains more than one item that matches, an <see cref="InvalidOperationException"/> is
+        /// thrown.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">There was more than one matching item in the sequence.</exception>
         public static IMaybe<T> SingleAsMaybe<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
             var firstTwoMatching = source.Where(predicate).Take(2).ToFinalList();
@@ -66,6 +87,10 @@ namespace Belt.Maybe
             }
         }
 
+        /// <summary>
+        /// Reduces an <c>IEnumerable</c> of <see cref="IMaybe{T}"/> to an <see cref="IEnumerable{T}"/>, where
+        /// existing maybes from the original sequence are kept and empty ones are left out.
+        /// </summary>
         public static IEnumerable<T> ExistingOnly<T>(this IEnumerable<IMaybe<T>> source)
         {
             return source.SelectMany(m => m.AsList());
