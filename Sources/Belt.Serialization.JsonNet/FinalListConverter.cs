4535aaaca071a6fb5c19604e314a22f875fead55
Dani Michel <dani.michel@smartprimes.ch>
2014-04-01 14:36:56 +0200

    FinalListConverter created.

---
+++ Sources/Belt.Serialization.JsonNet/FinalListConverter.cs
@@ -0,0 +1,56 @@
+﻿namespace Belt.Serialization.JsonNet
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Reflection;
+
+    using Belt.FinalList;
+
+    using Newtonsoft.Json;
+
+    public class FinalListConverter : JsonConverter
+    {
+        private static readonly Type _finalListMarkerType = typeof(IFinalList);
+
+        private static readonly Type _immutableListType = typeof(ImmutableList<>);
+
+        private static readonly MethodInfo _genericFinalListCreatorMethod = typeof(FinalListExtensions).GetMethod("ToFinalList");
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var enumerable = value as IEnumerable;
+            if (enumerable == null)
+            {
+                throw new ArgumentException("Value is not an IEnumerable, cannot serialize", "value");
+            }
+
+            writer.WriteStartArray();
+
+            foreach (var val in enumerable)
+            {
+                serializer.Serialize(writer, val);
+            }
+
+            writer.WriteEndArray();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var nativeType = _immutableListType.MakeGenericType(objectType.GenericTypeArguments);
+            var deserialize = serializer.Deserialize(reader, nativeType);
+
+            var creatorMethod = _genericFinalListCreatorMethod.MakeGenericMethod(objectType.GenericTypeArguments);
+            var finalList = creatorMethod.Invoke(null, new[] { deserialize });
+
+            return finalList;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == _finalListMarkerType || objectType.GetInterfaces().Any(i => i == _finalListMarkerType);
+        }
+    }
+}
---
+++ Sources/BeltTest/Belt.Serialization.JsonNet/FinalListSerializerTest.cs
@@ -0,0 +1,77 @@
+﻿namespace BeltTest.Belt.Serialization.JsonNet
+{
+    using System.Diagnostics;
+
+    using global::Belt.FinalList;
+    using global::Belt.Serialization.JsonNet;
+
+    using Newtonsoft.Json;
+
+    using Xunit;
+
+    public class FinalListSerializerTest
+    {
+        private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
+        {
+            Converters = new JsonConverter[] { new FinalListConverter() }
+        };
+
+        [Fact]
+        public void Can_roundtrip_value_typed_final_list_with_content()
+        {
+            var testObj = new Test<int> { TheList = FinalList.Create(42, 43) };
+
+            var deserialized = Roundtrip(testObj);
+
+            Assert.Equal(2, deserialized.TheList.Count);
+            Assert.Equal(42, deserialized.TheList[0]);
+            Assert.Equal(43, deserialized.TheList[1]);
+        }
+
+        [Fact]
+        public void Can_roundtrip_empty_value_typed_final_list()
+        {
+            var testObj = new Test<int> { TheList = FinalList.Empty<int>() };
+
+            var deserialized = Roundtrip(testObj);
+
+            Assert.Equal(0, deserialized.TheList.Count);
+        }
+
+        [Fact]
+        public void Can_roundtrip_reference_typed_final_list_with_content()
+        {
+            var testObj = new Test<string> { TheList = FinalList.Create("Hello", "Belt") };
+
+            var deserialized = Roundtrip(testObj);
+
+            Assert.Equal(2, deserialized.TheList.Count);
+            Assert.Equal("Hello", deserialized.TheList[0]);
+            Assert.Equal("Belt", deserialized.TheList[1]);
+        }
+
+        [Fact]
+        public void Can_roundtrip_empty_reference_typed_final_list()
+        {
+            var testObj = new Test<string> { TheList = FinalList.Empty<string>() };
+
+            var deserialized = Roundtrip(testObj);
+
+            Assert.Equal(0, deserialized.TheList.Count);
+        }
+
+        private T Roundtrip<T>(T testObj)
+        {
+            var json = JsonConvert.SerializeObject(testObj, _jsonSerializerSettings);
+
+            Debug.WriteLine(json);
+
+            return JsonConvert.DeserializeObject<T>(json, _jsonSerializerSettings);
+        }
+
+        private class Test<T>
+        {
+            public IFinalList<T> TheList { get; set; } 
+        }
+    }
+}
