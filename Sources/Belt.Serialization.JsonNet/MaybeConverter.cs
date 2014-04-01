bd3027f2bac3eed4c1ae233494378fec12ebe472
Dani Michel <dani.michel@smartprimes.ch>
2014-04-01 14:20:27 +0200

    MaybeConverter created.

---
+++ Sources/Belt.Serialization.JsonNet/MaybeConverter.cs
@@ -0,0 +1,51 @@
+﻿namespace Belt.Serialization.JsonNet
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    using Belt.Maybe;
+
+    using Newtonsoft.Json;
+
+    public class MaybeConverter : JsonConverter
+    {
+        private static readonly Type _markerType = typeof(IMaybe);
+
+        private static readonly MethodInfo _emptyCreatorMethod = typeof(Maybe).GetMethod("Empty");
+        private static readonly MethodInfo _existingCreatorMethod = typeof(Maybe).GetMethod("Is");
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var maybe = (IMaybe)value;
+
+            if (maybe.Exists)
+            {
+                var innerValue = maybe.GetType().GetProperty("It").GetValue(maybe);
+                serializer.Serialize(writer, innerValue);
+            }
+            else
+            {
+                writer.WriteNull();
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var nestedType = objectType.GenericTypeArguments.Single();
+
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return _emptyCreatorMethod.MakeGenericMethod(nestedType).Invoke(null, new object[] { });
+            }
+
+            var nestedValue = serializer.Deserialize(reader, nestedType);
+            return _existingCreatorMethod.MakeGenericMethod(nestedType).Invoke(null, new[] { nestedValue });
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == _markerType || objectType.GetInterfaces().Any(i => i == _markerType);
+        }
+    }
+}
---
+++ Sources/BeltTest/Belt.Serialization.JsonNet/MaybeSerializerTest.cs
@@ -0,0 +1,80 @@
+﻿namespace BeltTest.Belt.Serialization.JsonNet
+{
+    using System.Diagnostics;
+
+    using global::Belt.Maybe;
+    using global::Belt.Serialization.JsonNet;
+
+    using Newtonsoft.Json;
+
+    using Xunit;
+
+    public class MaybeSerializerTest
+    {
+        private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
+        {
+            Converters = new JsonConverter[] { new MaybeConverter() }
+        };
+
+        [Fact]
+        public void Empty_maybe_roundtrips_successfully_to_json()
+        {
+            var testObj = new TestRefType { TheMaybe = Maybe.Empty<string>() };
+
+            var deserialized = PerformRoundtrip(testObj);
+
+            Assert.True(deserialized.TheMaybe.IsEmpty);
+        }
+
+        [Fact]
+        public void Existing_maybe_roundtrips_successfully_to_json()
+        {
+            var testObj = new TestRefType { TheMaybe = Maybe.Is("Hello maybe!") };
+
+            var deserialized = PerformRoundtrip(testObj);
+
+            Assert.True(deserialized.TheMaybe.Exists);
+            Assert.Equal("Hello maybe!", deserialized.TheMaybe.It);
+        }
+
+        [Fact]
+        public void Empty_value_maybe_roundtrips_successfully_to_json()
+        {
+            var testObj = new TestValueType { TheMaybe = Maybe.Empty<int>() };
+
+            var deserialized = PerformRoundtrip(testObj);
+
+            Assert.True(deserialized.TheMaybe.IsEmpty);
+        }
+
+        [Fact]
+        public void Existing_value_maybe_roundtrips_successfully_to_json()
+        {
+            var testObj = new TestValueType { TheMaybe = Maybe.Is(42) };
+
+            var deserialized = PerformRoundtrip(testObj);
+
+            Assert.True(deserialized.TheMaybe.Exists);
+            Assert.Equal(42, deserialized.TheMaybe.It);
+        }
+
+        private T PerformRoundtrip<T>(T testObj)
+        {
+            var json = JsonConvert.SerializeObject(testObj, _jsonSerializerSettings);
+
+            Debug.WriteLine(json);
+
+            return JsonConvert.DeserializeObject<T>(json, _jsonSerializerSettings);
+        }
+
+        private class TestRefType
+        {
+            public IMaybe<string> TheMaybe { get; set; }
+        }
+
+        private class TestValueType
+        {
+            public IMaybe<int> TheMaybe { get; set; }
+        }
+    }
+}
