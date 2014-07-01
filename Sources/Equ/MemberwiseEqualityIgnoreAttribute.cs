04e949ea99d10b960b850f68e6f9d9d2ba2563ad
Dani Michel <dani.michel@smartprimes.ch>
2014-07-01 13:46:31 +0200

    The MemberwiseEqualityIgnoreAttribute can now also be set on properties.

--- Sources/Equ/MemberwiseEqualityIgnoreAttribute.cs
+++ Sources/Equ/MemberwiseEqualityIgnoreAttribute.cs
@@ -6,7 +6,7 @@ namespace Equ
     /// Tells the <see cref="MemberwiseEqualityComparer{T}" /> to ignore a specific field when performing
     /// the comparison. The field is also excluded from the hashcode computation.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
     public class MemberwiseEqualityIgnoreAttribute : Attribute
     {
     }
