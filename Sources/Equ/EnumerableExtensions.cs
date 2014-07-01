f717424cb79ab4f9aa76eb61d4ee4b530be53c37
Dani Michel <dani.michel@smartprimes.ch>
2014-07-01 17:49:49 +0200

    EquatableSequenceWrapper restored, but marked obsolete. This prevents breaking upgrades from Belt to Equ.

---
+++ Sources/Equ/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Equ
+{
+    using System;
+    using System.Collections.Generic;
+
+    // TODO Remove before stable version 
+    [Obsolete]
+    public static class EnumerableExtensions
+    {
+        [Obsolete]
+        public static EquatableSequenceWrapper<T> AsEquatableSequence<T>(this IEnumerable<T> source)
+        {
+            return new EquatableSequenceWrapper<T>(source);
+        }
+    }
+}
---
+++ Sources/Equ/EquatableSequenceWrapper.cs
@@ -0,0 +1,52 @@
+﻿namespace Equ
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    // TODO Remove before stable version 
+    [Obsolete]
+    public class EquatableSequenceWrapper<T> : IEquatable<EquatableSequenceWrapper<T>>
+    {
+        private readonly IEnumerable<T> _enumerable;
+
+        public EquatableSequenceWrapper(IEnumerable<T> enumerable)
+        {
+            _enumerable = enumerable;
+        }
+
+        public bool Equals(EquatableSequenceWrapper<T> other)
+        {
+            return _enumerable.SequenceEqual(other._enumerable);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            return Equals((EquatableSequenceWrapper<T>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _enumerable.Aggregate(0, (current, item) => current ^ item.GetHashCode());
+        }
+
+        public override string ToString()
+        {
+            return "[ "
+                   + _enumerable.Aggregate("", (accu, item) => accu + item.ToString() + ", ")
+                       .TrimEnd(new[] { ' ', ',' }) + " ]";
+        }
+    }
+}
