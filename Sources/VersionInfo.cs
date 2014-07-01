8cb12abd20b66c38d3061452ffce2a028f59beef
Dani Michel <dani.michel@smartprimes.ch>
2014-07-01 17:56:15 +0200

    Version bumped, first standalone version.

--- Sources/VersionInfo.cs
+++ Sources/VersionInfo.cs
@@ -6,5 +6,5 @@
 
 [assembly: System.Reflection.AssemblyCompany("Dani Michel")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © 2014")]
-[assembly: System.Reflection.AssemblyVersion("0.8.3")]
-[assembly: System.Reflection.AssemblyInformationalVersion("Equ 0.8.3")]
+[assembly: System.Reflection.AssemblyVersion("0.9.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("Equ 0.9.0")]
