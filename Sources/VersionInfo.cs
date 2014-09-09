d299cc64f22b125f962aed9b49fabb93c1faa6d6
Dani Michel <dani.michel@smartprimes.ch>
2014-09-09 11:48:23 +0200

    Version bumped.

--- Sources/VersionInfo.cs
+++ Sources/VersionInfo.cs
@@ -7,6 +7,6 @@
 [assembly: System.Reflection.AssemblyCompany("Dani Michel")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © 2014")]
 
-[assembly: System.Reflection.AssemblyVersion("0.9")]
-[assembly: System.Reflection.AssemblyFileVersion("0.9.1")]
-[assembly: System.Reflection.AssemblyInformationalVersion("0.9.1")]
+[assembly: System.Reflection.AssemblyVersion("0.9.2")]
+[assembly: System.Reflection.AssemblyFileVersion("0.9.2")]
+[assembly: System.Reflection.AssemblyInformationalVersion("0.9.2")]
