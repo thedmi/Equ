1957b86315cb98d3331e9beeb5566fb145a3d5f8
Zavarkó Gábor <zavarkog@gmail.com>
2017-11-01 13:05:16 +0100

    ElementwiseSequenceEqualityComparer fix when element is null

--- Sources/Equ.Test/ElementwiseSequenceEqualityComparerTest.cs
+++ Sources/Equ.Test/ElementwiseSequenceEqualityComparerTest.cs
@@ -35,5 +35,33 @@
             Assert.False(comparer.Equals(e1, e2));
             Assert.False(comparer.Equals(e2, e1));
         }
+
+        [Fact]
+        public void Null_values_are_reported_equal()
+        {
+            var e1 = new string[] { null };
+            var e2 = new List<string> { null };
+
+            var comparer = new ElementwiseSequenceEqualityComparer<IEnumerable<string>>();
+
+            Assert.True(comparer.Equals(e1, e1));
+            Assert.True(comparer.Equals(e1, e2));
+            Assert.True(comparer.Equals(e2, e1));
+            Assert.True(comparer.Equals(e2, e2));
+
+            Assert.Equal(comparer.GetHashCode(e1), comparer.GetHashCode(e2));
+        }
+
+        [Fact]
+        public void Null_and_non_null_values_are_reported_different()
+        {
+            var e1 = new[] { "abc" };
+            var e2 = new List<string> { null };
+
+            var comparer = new ElementwiseSequenceEqualityComparer<IEnumerable<string>>();
+
+            Assert.False(comparer.Equals(e1, e2));
+            Assert.False(comparer.Equals(e2, e1));
+        }
     }
 }
