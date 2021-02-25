819582125e5cb8f3827ecc287aa5f7c8bff69f4f
ibebbs <github@bebbs.co.uk>
2021-02-25 21:24:47 +0000

    All tests passing.

--- Sources/Equ/ElementwiseSequenceEqualityComparer.cs
+++ Sources/Equ/ElementwiseSequenceEqualityComparer.cs
@@ -14,17 +14,70 @@
     /// <typeparam name="T">The type of the enumerable, i.e. a type implementing <see cref="IEnumerable"/></typeparam>
     public class ElementwiseSequenceEqualityComparer<T> : EqualityComparer<T> where T : IEnumerable
     {
-#if NETSTANDARD2_0
-        private static readonly MethodInfo SequenceEqualsMethodInfo = typeof(Enumerable)
-            .GetMethods(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public)
-            .Where(methodInfo => methodInfo.Name.Equals(nameof(Enumerable.SequenceEqual)) && methodInfo.GetParameters().Length == 3)
-            .Single();
+#if NETSTANDARD1_5
+        private static Type GetEnumerableType()
+        {
+            var result = typeof(T).GetTypeInfo().GetInterfaces()
+                .Where(type => type.GetTypeInfo().IsGenericType && typeof(IEnumerable<>).GetTypeInfo().IsAssignableFrom(type.GetGenericTypeDefinition()))
+                .SelectMany(type => type.GetTypeInfo().GetGenericArguments())
+                .SingleOrDefault();
 
-        private static readonly Type EnumerableType = typeof(T).IsGenericType ? typeof(T).GetGenericArguments().Single() : null;
-        private static readonly Type MemberwiseEqualityComparerType = typeof(MemberwiseEqualityComparer<>);
+            return result;
+        }
 #else
-        private static readonly Type EnumerableType = null;
+        private static Type GetEnumerableType()
+        {
+            var result = typeof(T).GetInterfaces()
+                .Where(type => type.IsGenericType && typeof(IEnumerable<>).IsAssignableFrom(type.GetGenericTypeDefinition()))
+                .SelectMany(type => type.GetGenericArguments())
+                .SingleOrDefault();
+
+            return result;
+        }
 #endif
+
+        private static readonly Type EnumerableType = GetEnumerableType();
+
+#if NETSTANDARD1_5
+        private static MethodInfo GetSequenceEqualsMethodInfo()
+        {
+            var result = typeof(Enumerable)
+                .GetTypeInfo()
+                .GetMethods(BindingFlags.Static | BindingFlags.Public)
+                .Where(methodInfo => methodInfo.Name.Equals(nameof(Enumerable.SequenceEqual)) && methodInfo.GetParameters().Length == 3)
+                .Single();
+
+            return result;
+        }
+#else
+        private static MethodInfo GetSequenceEqualsMethodInfo()
+        {
+            var result = typeof(Enumerable)
+                .GetMethods(BindingFlags.Static | BindingFlags.Public)
+                .Where(methodInfo => methodInfo.Name.Equals(nameof(Enumerable.SequenceEqual)) && methodInfo.GetParameters().Length == 3)
+                .Single();
+
+            return result;
+        }
+#endif
+
+        private static readonly MethodInfo SequenceEqualsMethodInfo = GetSequenceEqualsMethodInfo();
+
+        private static MethodInfo GetScrambledEqualsMethodInfo()
+        {
+            var result = typeof(ElementwiseSequenceEqualityComparer<T>)
+                .GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
+                .Where(methodInfo => methodInfo.Name.Equals(nameof(ElementwiseSequenceEqualityComparer<T>.ScrambledEquals)) && methodInfo.IsGenericMethod)
+                .Single();
+
+            return result;
+        }
+
+        private static readonly MethodInfo ScrambledEqualsMethodInfo = GetScrambledEqualsMethodInfo();
+
+        private static readonly Type MemberwiseEqualityComparerType = typeof(MemberwiseEqualityComparer<>);
+#endif
+
 
 
         // ReSharper disable once UnusedMember.Global
@@ -136,17 +189,39 @@
             return type.IsGenericType && typeof(ISet<>).GetTypeInfo().IsAssignableFrom(type.GetGenericTypeDefinition());
         }
 
+#if NETSTANDARD2_0
+        private static bool ScrambledEquals(IEnumerable left, IEnumerable right)
+        {
+            if (EnumerableType != null && !EnumerableType.IsPrimitive)
+            {
+                var equalityComparerType = MemberwiseEqualityComparerType.MakeGenericType(EnumerableType);
+                var equalityComparer = equalityComparerType.GetProperty(nameof(MemberwiseEqualityComparer<object>.ByProperties), BindingFlags.Static | BindingFlags.Public).GetValue(null);
+                var scrambledEquals = ScrambledEqualsMethodInfo.MakeGenericMethod(EnumerableType);
+                var result = (bool) scrambledEquals.Invoke(null, new [] { left, right, equalityComparer }); 
+
+                return result;
+            }
+            else
+            {
+                var leftEnumerable = left.Cast<object>();
+                var rightEnumerable = right.Cast<object>();
+
+                return ScrambledEquals(leftEnumerable, rightEnumerable, EqualityComparer<object>.Default);
+            }
+        }
+#else
         private static bool ScrambledEquals(IEnumerable left, IEnumerable right)
         {
             var leftEnumerable = left.Cast<object>();
             var rightEnumerable = right.Cast<object>();
 
-            return ScrambledEquals(leftEnumerable, rightEnumerable);
+            return ScrambledEquals(leftEnumerable, rightEnumerable, EqualityComparer<object>.Default);
         }
+#endif
 
-        private static bool ScrambledEquals<TElem>(IEnumerable<TElem> list1, IEnumerable<TElem> list2)
+        private static bool ScrambledEquals<TElem>(IEnumerable<TElem> list1, IEnumerable<TElem> list2, IEqualityComparer<TElem> equalityComparer)
         {
-            var counters = new Dictionary<TElem, int>();
+            var counters = new Dictionary<TElem, int>(equalityComparer);
             foreach (var element in list1)
             {
                 if (counters.ContainsKey(element))
