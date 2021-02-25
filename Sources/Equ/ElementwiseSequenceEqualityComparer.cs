1b8d57682cc0c5efd09da178cc2c7c4dae1b248f
ibebbs <github@bebbs.co.uk>
2021-02-25 21:28:41 +0000

    Removed all conditional compilation directives

--- Sources/Equ/ElementwiseSequenceEqualityComparer.cs
+++ Sources/Equ/ElementwiseSequenceEqualityComparer.cs
@@ -14,7 +14,6 @@
     /// <typeparam name="T">The type of the enumerable, i.e. a type implementing <see cref="IEnumerable"/></typeparam>
     public class ElementwiseSequenceEqualityComparer<T> : EqualityComparer<T> where T : IEnumerable
     {
-#if NETSTANDARD1_5
         private static Type GetEnumerableType()
         {
             var result = typeof(T).GetTypeInfo().GetInterfaces()
@@ -24,21 +23,9 @@
 
             return result;
         }
-#else
-        private static Type GetEnumerableType()
-        {
-            var result = typeof(T).GetInterfaces()
-                .Where(type => type.IsGenericType && typeof(IEnumerable<>).IsAssignableFrom(type.GetGenericTypeDefinition()))
-                .SelectMany(type => type.GetGenericArguments())
-                .SingleOrDefault();
-
-            return result;
-        }
-#endif
 
         private static readonly Type EnumerableType = GetEnumerableType();
 
-#if NETSTANDARD1_5
         private static MethodInfo GetSequenceEqualsMethodInfo()
         {
             var result = typeof(Enumerable)
@@ -49,23 +36,13 @@
 
             return result;
         }
-#else
-        private static MethodInfo GetSequenceEqualsMethodInfo()
-        {
-            var result = typeof(Enumerable)
-                .GetMethods(BindingFlags.Static | BindingFlags.Public)
-                .Where(methodInfo => methodInfo.Name.Equals(nameof(Enumerable.SequenceEqual)) && methodInfo.GetParameters().Length == 3)
-                .Single();
-
-            return result;
-        }
-#endif
 
         private static readonly MethodInfo SequenceEqualsMethodInfo = GetSequenceEqualsMethodInfo();
 
         private static MethodInfo GetScrambledEqualsMethodInfo()
         {
             var result = typeof(ElementwiseSequenceEqualityComparer<T>)
+                .GetTypeInfo()
                 .GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
                 .Where(methodInfo => methodInfo.Name.Equals(nameof(ElementwiseSequenceEqualityComparer<T>.ScrambledEquals)) && methodInfo.IsGenericMethod)
                 .Single();
@@ -76,8 +53,6 @@
         private static readonly MethodInfo ScrambledEqualsMethodInfo = GetScrambledEqualsMethodInfo();
 
         private static readonly Type MemberwiseEqualityComparerType = typeof(MemberwiseEqualityComparer<>);
-#endif
-
 
 
         // ReSharper disable once UnusedMember.Global
@@ -104,13 +79,12 @@
             return _typeHasDefinedOrder ? SequenceEqual(left, right) : ScrambledEquals(left, right);
         }
 
-#if NETSTANDARD2_0
         private bool SequenceEqual(IEnumerable left, IEnumerable right)
         {
             if (EnumerableType != null)
             {
                 var equalityComparerType = MemberwiseEqualityComparerType.MakeGenericType(EnumerableType);
-                var equalityComparer = equalityComparerType.GetProperty(nameof(MemberwiseEqualityComparer<object>.ByProperties), BindingFlags.Static | BindingFlags.Public).GetValue(null);
+                var equalityComparer = equalityComparerType.GetTypeInfo().GetProperty(nameof(MemberwiseEqualityComparer<object>.ByProperties), BindingFlags.Static | BindingFlags.Public).GetValue(null);
                 var sequenceEquals = SequenceEqualsMethodInfo.MakeGenericMethod(EnumerableType);
                 var result = (bool)sequenceEquals.Invoke(null, new [] { left, right, equalityComparer });
 
@@ -123,14 +97,7 @@
                 return leftEnumerable.SequenceEqual(rightEnumerable);
             }
         }
-#else
-        private bool SequenceEqual(IEnumerable left, IEnumerable right)
-        {
-            var leftEnumerable = left.Cast<object>();
-            var rightEnumerable = right.Cast<object>();
-            return leftEnumerable.SequenceEqual(rightEnumerable);
-        }
-#endif
+
         public override int GetHashCode(T obj)
         {
             if (ReferenceEquals(null, obj))
@@ -189,13 +156,12 @@
             return type.IsGenericType && typeof(ISet<>).GetTypeInfo().IsAssignableFrom(type.GetGenericTypeDefinition());
         }
 
-#if NETSTANDARD2_0
         private static bool ScrambledEquals(IEnumerable left, IEnumerable right)
         {
-            if (EnumerableType != null && !EnumerableType.IsPrimitive)
+            if (EnumerableType != null && !EnumerableType.GetTypeInfo().IsPrimitive)
             {
                 var equalityComparerType = MemberwiseEqualityComparerType.MakeGenericType(EnumerableType);
-                var equalityComparer = equalityComparerType.GetProperty(nameof(MemberwiseEqualityComparer<object>.ByProperties), BindingFlags.Static | BindingFlags.Public).GetValue(null);
+                var equalityComparer = equalityComparerType.GetTypeInfo().GetProperty(nameof(MemberwiseEqualityComparer<object>.ByProperties), BindingFlags.Static | BindingFlags.Public).GetValue(null);
                 var scrambledEquals = ScrambledEqualsMethodInfo.MakeGenericMethod(EnumerableType);
                 var result = (bool) scrambledEquals.Invoke(null, new [] { left, right, equalityComparer }); 
 
@@ -209,15 +175,6 @@
                 return ScrambledEquals(leftEnumerable, rightEnumerable, EqualityComparer<object>.Default);
             }
         }
-#else
-        private static bool ScrambledEquals(IEnumerable left, IEnumerable right)
-        {
-            var leftEnumerable = left.Cast<object>();
-            var rightEnumerable = right.Cast<object>();
-
-            return ScrambledEquals(leftEnumerable, rightEnumerable, EqualityComparer<object>.Default);
-        }
-#endif
 
         private static bool ScrambledEquals<TElem>(IEnumerable<TElem> list1, IEnumerable<TElem> list2, IEqualityComparer<TElem> equalityComparer)
         {
